0d01c4974bff202fd6f36f99f1a7628788f26444
Mario <mariohdpz@gmail.com>
2020-10-17 19:22:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Financial Life
+
+Equivócate en el juego y no en la vida real
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Financial Life.
+        </h2>
+        <p>
+          Equivócate en el juego y no en la vida real.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
